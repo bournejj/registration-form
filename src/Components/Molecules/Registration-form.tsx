d34ecd2e08096682ca903825ee4b04aa65692ffd
Jack Bourne <jackbourne@Jacks-MacBook-Pro.local>
2025-07-24 16:57:52 +0100

    reinstalled tailwindcss as many had been installed causing issues

---
+++ src/Components/Molecules/Registration-form.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface registrationformProps {
+}
+
+const RegistrationForm: React.FC<registrationformProps> = ({}) => {
+    return (
+        <div> <h1 className='bg-black
+        '>form</h1></div>
+    )
+}
+
+export default RegistrationForm;
